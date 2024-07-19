29a4b2ed02b396c8a16932ce147f1250b934aa44
fkihay <fkihaikal568@gmail.com>
2024-07-19 23:22:29 +0700

    feet: realtime fix ajax polling with schedule command

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -45,7 +45,7 @@
 
     <!-- Page Specific JS -->
     <script src="{{asset("js/app.js")}}"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    {{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> --}}
 
 </body>
 </html>
